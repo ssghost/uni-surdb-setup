d540fd72c7ae0a63dab788c3efeb8fcc32621a4c
Stardust Song <6882971+ssghost@users.noreply.github.com>
2024-01-31 22:49:27 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -2,13 +2,19 @@ from surrealdb import Surreal
 import pandas as pd
 import asyncio
 
+def dec_utf(x):
+    return x.decode('utf-8')
+    
 async def upload():
-    df = pd.read_parquet("./uniswap_v3_pools.parquet").to_json()
-    async with Surreal("ws://localhost:8000/rpc") as db:
-        await db.signin({"user": "root", "pass": "root"})
-        await db.use("uniswap", "uniswap")
-        await db.delete("uniswap_v3_pools")
-        await db.create(df)
+    for i in range(9):    
+        df = pd.read_parquet(f"./uniswap_v3_pools_{str(i)}.parquet")
+        df["event__pool"].apply(dec_utf)
+        df.to_json(default_handler=str)
+        async with Surreal("ws://localhost:8000/rpc") as db:
+            await db.signin({"user": "root", "pass": "root"})
+            await db.use("uniswap", "uniswap")
+            await db.delete("uniswap_v3_pools_{str(i)}")
+            await db.create(df)
 
 if __name__ == "__main__":
     asyncio.run(upload())
